1a49437b645dc0f9ae8d834ec0e924c6a36a28e6
David Němeček <david.nemecek@unob.cz>
2023-11-26 16:03:03 +0100

    Created withInfo.py from GraphTypeDefinitions.py

---
+++ gql_presences/GraphTypeDefinitions/withInfo.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def withInfo(info):
+    asyncSessionMaker = info.context["asyncSessionMaker"]
+    async with asyncSessionMaker() as session:
+        try:
+            yield session
+        finally:
+            pass
+
+
+def AsyncSessionFromInfo(info):
+    print(
+        "obsolete function used AsyncSessionFromInfo, use withInfo context manager instead"
+    )
+    return info.context["session"]
+
