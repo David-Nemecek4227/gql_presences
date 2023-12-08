638aff98d61ed05988cac03fc310e94ae4983037
David Němeček <david.nemecek@unob.cz>
2023-12-08 08:42:43 +0100

    Updated UDBFeeder.py to use UUID

--- gql_presences/DBFeeder.py
+++ gql_presences/DBFeeder.py
@@ -158,36 +158,34 @@ import os
 import json
 from uoishelpers.feeders import ImportModels
 import datetime
+import uuid
 
 def get_demodata():
     def datetime_parser(json_dict):
         for (key, value) in json_dict.items():
-            result = value
-            try:
-                dateValue = datetime.datetime.fromisoformat(value)
-                result = dateValue.replace(tzinfo=None)
-            except :
-                #print(key, value)
-                pass
-            json_dict[key] = result
+            if key in ["startdate", "enddate", "lastchange", "created"]:
+                if value is None:
+                    dateValueWOtzinfo = None
+                else:
+                    try:
+                        dateValue = datetime.datetime.fromisoformat(value)
+                        dateValueWOtzinfo = dateValue.replace(tzinfo=None)
+                    except:
+                        print("jsonconvert Error", key, value, flush=True)
+                        dateValueWOtzinfo = None
 
-            # if key in ["startdate", "enddate", "lastchange", "created", 
-            #            "date_of_entry",
-            #             "date_of_fulfillment",
-            #             "date_of_submission"]:
-            #     if value is None:
-            #         dateValueWOtzinfo = None
-            #     else:
-            #         try:
-            #             dateValue = datetime.datetime.fromisoformat(value)
-            #             dateValueWOtzinfo = dateValue.replace(tzinfo=None)
-            #         except:
-            #             print("jsonconvert Error", key, value, flush=True)
-            #             dateValueWOtzinfo = None
-                
-            #     json_dict[key] = dateValueWOtzinfo
+                json_dict[key] = dateValueWOtzinfo
+
+            if (key in ["id", "changedby", "createdby"]) or ("_id" in key):
+
+                if key == "outer_id":
+                    json_dict[key] = value
+                elif value not in ["", None]:
+                    json_dict[key] = uuid.UUID(value)
+                else:
+                    print(key, value)
+
         return json_dict
-
 
     with open("./systemdata.json", "r") as f:
         jsonData = json.load(f, object_hook=datetime_parser)
@@ -199,6 +197,8 @@ async def initDB(asyncSessionMaker):
     defaultNoDemo = "False"
     if defaultNoDemo == os.environ.get("DEMO", defaultNoDemo):
         dbModels = [
+            ContentModel,
+            TaskModel,
         ]
     else:
         dbModels = [
