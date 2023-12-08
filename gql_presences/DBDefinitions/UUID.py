2563ea6b1bc1a115ea3c4daf10d6ea4175797f5a
David Němeček <david.nemecek@unob.cz>
2023-12-08 08:32:33 +0100

    Updated UUID.py to use uuid

--- gql_presences/DBDefinitions/UUID.py
+++ gql_presences/DBDefinitions/UUID.py
@@ -1,29 +1,10 @@
-import uuid
+from uuid import uuid4, UUID
+from sqlalchemy import Column, Uuid
+uuid = uuid4
 
-from sqlalchemy import (
-    Column,
-    String
-)
+def UUIDColumn():
+    return Column(Uuid, primary_key=True, comment="primary key", default=uuid)
 
-def newUuidAsString():
-    return f"{uuid.uuid1()}"
-
-def UUIDColumn(name=None):
-    if name is None:
-        return Column(String, primary_key=True, unique=True, default=newUuidAsString)
-    else:
-        return Column(
-            name, String, primary_key=True, unique=True, default=newUuidAsString
-        )
-
-def UUIDFKey(*, ForeignKey=None, nullable=False):
-    if ForeignKey is None:
-        return Column(
-            String, index=True, nullable=nullable
-        )
-    else:
-        return Column(
-            ForeignKey, index=True, nullable=nullable
-        )
-
+def UUIDFKey(comment=None, nullable=True, **kwargs):
+    return Column(Uuid, index=True, comment=comment, nullable=nullable, **kwargs)
 # id = Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("uuid_generate_v4()"),)
