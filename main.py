45c26e51182a035a8a71228b28f48493828c5c5e
David Němeček <david.nemecek@unob.cz>
2024-01-14 23:00:11 +0100

    complete overhaul of main.py

--- main.py
+++ main.py
@@ -1,129 +1,121 @@
-from typing import List
-import typing
+import logging
 
-import asyncio
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s.%(msecs)03d\t%(levelname)s:\t%(message)s',
+    datefmt='%Y-%m-%dT%I:%M:%S')
 
-from fastapi import FastAPI
-import strawberry as strawberryA
+from fastapi import FastAPI, Request
+import strawberry
 from strawberry.fastapi import GraphQLRouter
+from contextlib import asynccontextmanager
 
 ## Definice GraphQL typu (pomoci strawberry https://strawberry.rocks/)
 ## Strawberry zvoleno kvuli moznosti mit federovane GraphQL API (https://strawberry.rocks/docs/guides/federation, https://www.apollographql.com/docs/federation/)
-#from gql_presences.GraphTypeDefinitions.Query import Query
 
 ## Definice DB typu (pomoci SQLAlchemy https://www.sqlalchemy.org/)
 ## SQLAlchemy zvoleno kvuli moznost komunikovat s DB asynchronne
 ## https://docs.sqlalchemy.org/en/14/core/future.html?highlight=select#sqlalchemy.future.select
-from gql_presences.DBDefinitions import startEngine, ComposeConnectionString
-from gql_presences.DBFeeder import initDB
+from DBDefinitions import ComposeConnectionString
 
 ## Zabezpecuje prvotni inicializaci DB a definovani Nahodne struktury pro "Univerzity"
 # from gql_workflow.DBFeeder import createSystemDataStructureRoleTypes, createSystemDataStructureGroupTypes
 
 connectionString = ComposeConnectionString()
 
-
-def singleCall(asyncFunc):
-    """Dekorator, ktery dovoli, aby dekorovana funkce byla volana (vycislena) jen jednou. Navratova hodnota je zapamatovana a pri dalsich volanich vracena.
-    Dekorovana funkce je asynchronni.
-    """
-    resultCache = {}
-
-    async def result():
-        if resultCache.get("result", None) is None:
-            resultCache["result"] = await asyncFunc()
-        return resultCache["result"]
-
-    return result
+appcontext = {}
 
 
-@singleCall
-async def RunOnceAndReturnSessionMaker():
-    """Provadi inicializaci asynchronniho db engine, inicializaci databaze a vraci asynchronni SessionMaker.
-    Protoze je dekorovana, volani teto funkce se provede jen jednou a vystup se zapamatuje a vraci se pri dalsich volanich.
-    """
-    print(f'starting engine for "{connectionString}"')
+@asynccontextmanager
+async def initEngine(app: FastAPI):
+    from DBDefinitions import startEngine, ComposeConnectionString
 
-    import os
-    makeDrop = os.environ.get("DEMO", "") == "true"
-    result = await startEngine(
-        connectionstring=connectionString, makeDrop=makeDrop, makeUp=True
+    connectionstring = ComposeConnectionString()
+
+    asyncSessionMaker = await startEngine(
+        connectionstring=connectionstring,
+        makeDrop=True,
+        makeUp=True
     )
 
-    print(f"initializing system structures")
+    appcontext["asyncSessionMaker"] = asyncSessionMaker
 
-    ###########################################################################################################################
-    #
-    # zde definujte do funkce asyncio.gather
-    # vlozte asynchronni funkce, ktere maji data uvest do prvotniho konzistentniho stavu
+    logging.info("engine started")
 
-    # await asyncio.gather( # concurency running :)
-    # sem lze dat vsechny funkce, ktere maji nejak inicializovat databazi
-    # musi byt asynchronniho typu (async def ...)
-    # createSystemDataStructureRoleTypes(result),
-    # createSystemDataStructureGroupTypes(result)
-    # )
-    await initDB(result)
-    ###########################################################################################################################
-    print(f"all done")
+    from utils.DBFeeder import initDB
+    await initDB(asyncSessionMaker)
+
+    logging.info("data (if any) imported")
+    yield
+
+
+from GraphTypeDefinitions import schema
+
+app = FastAPI(lifespan=initEngine)
+
+
+async def get_context():
+    asyncSessionMaker = appcontext.get("asyncSessionMaker", None)
+    if asyncSessionMaker is None:
+        async with initEngine(app) as cntx:
+            pass
+
+    from utils.Dataloaders import createLoadersContext
+    context = createLoadersContext(appcontext["asyncSessionMaker"])
+    result = {**context}
+    result["user"] = {"id": "2d9dc5ca-a4a2-11ed-b9df-0242ac120003"}
     return result
 
 
-from strawberry.asgi import GraphQL
-from gql_presences.Dataloaders import createLoaders
-
-class MyGraphQL(GraphQL):
-    """Rozsirena trida zabezpecujici praci se session"""
-
-    async def __call__(self, scope, receive, send):
-        asyncSessionMaker = await RunOnceAndReturnSessionMaker()
-        async with asyncSessionMaker() as session:
-            self._session = session
-            self._user = {"id": "?"}
-            return await GraphQL.__call__(self, scope, receive, send)
-
-    async def get_context(self, request, response):
-        parentResult = await GraphQL.get_context(self, request, response)
-        asyncSessionMaker = await RunOnceAndReturnSessionMaker()
-        return {
-            **parentResult,
-            "session": self._session,
-            "asyncSessionMaker": asyncSessionMaker,
-            "user": self._user,
-            "all": await createLoaders(asyncSessionMaker)
-        }
-
-import gql_presences.GraphTypeDefinitions
-## ASGI app, kterou "moutneme"
-from gql_presences.GraphTypeDefinitions.Query import Query
-from gql_presences.GraphTypeDefinitions.UserGQLModel import UserGQLModel
-from gql_presences.GraphTypeDefinitions.EventGQLModel import EventGQLModel
-from gql_presences.GraphTypeDefinitions.Mutation import Mutation
-
-schema = strawberryA.federation.Schema(Query, types=(UserGQLModel, EventGQLModel), mutation=Mutation)
-
-graphql_app = MyGraphQL(
-    schema, graphiql=True, allow_queries_via_get=True
+graphql_app = GraphQLRouter(
+    schema,
+    context_getter=get_context
 )
 
-app = FastAPI()
-app.mount("/gql", graphql_app)
+app.include_router(graphql_app, prefix="/gql")
 
-
-@app.on_event("startup")
-async def startup_event():
-    initizalizedEngine = await RunOnceAndReturnSessionMaker()
-    return None
 
 
 print("All initialization is done")
 
-# @app.get('/hello')
-# def hello():
-#    return {'hello': 'world'}
 
-###########################################################################################################################
-#
-# pokud jste pripraveni testovat GQL funkcionalitu, rozsirte apollo/server.js
-#
-###########################################################################################################################
+@app.get('/hello')
+def hello(request: Request):
+    headers = request.headers
+    auth = request.auth
+    user = request.scope["user"]
+    return {'hello': 'world', 'headers': {**headers}, 'auth': f"{auth}", 'user': user}
+
+
+# from starlette.authentication import (
+#     AuthCredentials, AuthenticationBackend
+# )
+# from starlette.middleware.authentication import AuthenticationMiddleware
+
+
+# class BasicAuthBackend(AuthenticationBackend):
+#     async def authenticate(self, conn):
+#         return AuthCredentials(["authenticated"]), {"name": "John", "surname": "Newbie"}
+
+
+# app.add_middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
+'''
+import os
+DEMO = os.getenv("DEMO", None)
+assert DEMO is not None, "DEMO environment variable must be explicitly defined"
+assert (DEMO == "True") or (DEMO == "False"), "DEMO environment variable can have only `True` or `False` values"
+DEMO = DEMO == "True"
+
+if DEMO:
+    print("####################################################")
+    print("#                                                  #")
+    print("# RUNNING IN DEMO                                  #")
+    print("#                                                  #")
+    print("####################################################")
+
+    logging.info("####################################################")
+    logging.info("#                                                  #")
+    logging.info("# RUNNING IN DEMO                                  #")
+    logging.info("#                                                  #")
+    logging.info("####################################################")
+'''
